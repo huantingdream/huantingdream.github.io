458d55d27a4927a1151cddce82d7c5a05f6d89e3
root <root@localhost.localdomain>
2024-11-24 00:36:54 +0800

    Site updated: 2024-11-24 00:36:54

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/11/23/hello-world/"];function toRandomPost(){
+var posts=["2024/11/23/hello-world/","2024/11/24/测试文章/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
