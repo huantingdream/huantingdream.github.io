7106d6776bd445bf02063a0104002da644f7b52c
root <root@localhost.localdomain>
2024-12-31 15:53:31 +0800

    Site updated: 2024-12-31 15:53:30

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/11/23/hello-world/","2024/11/24/测试文章/","2024/11/24/群晖套件版Jellyfin添加代理/"];function toRandomPost(){
+var posts=["2024/11/23/hello-world/","2024/11/24/群晖套件版Jellyfin添加代理/","2024/11/24/解决使用代理时无法访问UWP相关应用/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
